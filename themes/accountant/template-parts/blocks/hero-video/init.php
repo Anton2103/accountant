0479b27fdb3a7cb7097e4032b18105bb8ab7e71c
Anton2103 <gaitanzhy@gmail.com>
2024-10-04 11:45:50 +0300

    no message

---
+++ themes/accountant/template-parts/blocks/hero-video/init.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+	'name'        => 'hero-video',
+	'title'       => __('Hero video', 'accountant'),
+	'description' => '',
+	'category'    => 'layout',
+	'icon'        => 'embed-generic',
+	'align'       => 'center',
+	'mode'        => 'preview',
+	'supports'    => [
+		'align' => false,
+		'mode'  => true,
+	]
+];
